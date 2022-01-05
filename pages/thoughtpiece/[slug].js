5114eca70c42cf2170591a643215e9c9d3d2eab1
Georgiy-huma <georgiy.havrylenko@huma.com>
2022-01-05 13:48:25 +0200

    Refactor and cleanup
    - Separate component for blog view
    - Some cleanup

--- pages/thoughtpiece/[slug].js
+++ pages/thoughtpiece/[slug].js
@@ -1,67 +1,32 @@
-import { getBySlug } from '../../lib'
 import fs from 'fs'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { getBySlug } from '../../lib'
+
+import PostLayout from './post-layout'
 
 const Post = ({ postData }) => { //thoughtpiece
 
-
-  console.log('POST DATA', postData)
-
-
-  // const { header, subHeader, author, publishDate, minToRead, postContent} = postData
-
-
-  return <>
-  {postData
-    ?<>
-      <span>BLOG</span>
-      <h1>{postData.header}</h1>
-      <h2>{postData.subHeader}</h2>
-      <p>First published: {postData.publishDate} | {postData.minToRead} min read</p>
-      <hr />
-      <span><b>Author:</b> {postData.author}</span>
-      <ReactMarkdown>
-        {postData.postContent}
-      </ReactMarkdown>
-    </>
-    : <>Empty post</>
-  }
-  </>
+  return <PostLayout post={postData}/>
 }
 
 export async function getStaticPaths() {
 
   const files = fs.readdirSync('content/thoughtpiece')
-
-  // console.log('Files!', files)
-
   const listOfPaths = []
 
-  for ( let file of files) {
+  for (let file of files) {
     listOfPaths.push(`/thoughtpiece/${file.substring(0, file.length-3)}`)
   }
 
   return {
     paths: listOfPaths,
-    /* paths: [
-      // String variant:
-      '/thoughtpiece/sometesttitle',
-      // Object variant:
-      // { params: { slug: 'second-post' } },
-    ], */
-    fallback: true,
+    fallback: true
   }
 }
 
 export async function getStaticProps({ params }) {
 
-  const { slug } = params
-
-  console.log('Params! SLUG', slug)
-
+  const { slug } = params // get slug from route
   const postData = getBySlug('content/thoughtpiece', slug)
-
-  console.log('DATA----------', postData)
 
   return {
     props: {
---
+++ pages/thoughtpiece/post-layout.js
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from 'react'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+
+import GradientBackground from '../../components/pages/GradientBackground'
+
+import styles from '../../styles/thoughtpiece/post-layout.module.scss'
+
+const PostLayout = ({ post }) => {
+
+  const [ height, setHeight ] = useState(1000)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    setHeight(ref.current.clientHeight) // this should be a custom hook
+  }, [])
+
+  return <>
+    {post
+      ? <section ref={ref} className={styles.mainSection} style={{ height: '100vh' }}>
+      <GradientBackground height={height} />
+      <article className={styles.postContainer}>
+        <div className={styles.mainColumn}>
+          <span>BLOG</span>
+          <h1>{post.header}</h1>
+          <h2>{post.subHeader}</h2>
+
+          <p>First published: {post.publishDate} | {post.minToRead} min read</p>
+
+          <hr />
+
+          <p className={styles.authorInfo}>
+            <b>Author:</b> {post.author}
+          </p>
+          
+          <div className={styles.postContent}>
+            <ReactMarkdown>
+              {post.postContent}
+            </ReactMarkdown>
+          </div>
+
+        </div>
+      </article>
+    </section>
+    : <>Post data missing</>
+    }
+  </>
+}
+
+export default PostLayout
